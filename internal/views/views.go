d8197fbfa5943109ac04f73307cab0242bc0a137
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2023-12-13 14:20:26 +0100

    add bootstrap

--- internal/views/views.go
+++ internal/views/views.go
@@ -1,3 +1,25 @@
 package views
 
+import (
+	"embed"
+	"fmt"
+	"io/fs"
+	"net/http"
+)
+
 //go:generate templ generate
+
+//go:embed bootstrap-5.1.3-dist/*
+var FsBootstrap embed.FS
+
+func BootStrapHandler(subPathStripPrefix string) http.HandlerFunc {
+	bootstrapFS, err := fs.Sub(FsBootstrap, "bootstrap-5.1.3-dist")
+	if err != nil {
+		panic(fmt.Errorf("failed getting the sub tree for the site files: %w", err))
+	}
+	handler := http.FileServer(http.FS(bootstrapFS))
+	static := http.StripPrefix(subPathStripPrefix, handler)
+	return func(w http.ResponseWriter, r *http.Request) {
+		static.ServeHTTP(w, r)
+	}
+}
--- internal/webserver/routes.go
+++ internal/webserver/routes.go
@@ -10,19 +10,18 @@ import (
 
 func (w *Webserver) initRoutes() {
 	w.router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		// w.Write([]byte("hello world"))
 		err := views.Hello("world").Render(context.Background(), w)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
 	w.router.Get("/test", func(w http.ResponseWriter, r *http.Request) {
-		// w.Write([]byte("hello world"))
-		// Hello("world").Render(context.Background(), w)
 		w.Header().Set("Content-Type", "application/json")
 		err := json.NewEncoder(w).Encode(Msg{Name: "test", Message: "test"})
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
+
+	w.router.Handle("/bootstrap-5.1.3-dist/*", views.BootStrapHandler("/bootstrap-5.1.3-dist/"))
 }
